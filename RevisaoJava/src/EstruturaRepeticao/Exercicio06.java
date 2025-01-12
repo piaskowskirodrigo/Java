6b36589d7560ee30f724819db55123ce334ed739
piaskowskirodrigo <61170540+piaskowskirodrigo@users.noreply.github.com>
2025-01-12 20:25:06 -0300

    Exercicio

---
+++ RevisaoJava/src/EstruturaRepeticao/Exercicio06.java
@@ -0,0 +1,15 @@
+/*Faça um programa que imprima na tela os números de 1 a 20, um
+abaixo do outro. Depois modifique o programa para que ele mostre os
+números um ao lado do outro.*/
+
+package EstruturaRepeticao;
+
+public class Exercicio06 {
+	
+	public static void main(String[]args) {
+		for (int i = 0; i < 21; i++) {
+			System.out.print(i);
+		}
+	}
+
+}
