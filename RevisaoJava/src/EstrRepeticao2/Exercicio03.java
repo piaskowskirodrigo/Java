9d59584db598ee2799a06984db44cabaf70bba91
piaskowskirodrigo <61170540+piaskowskirodrigo@users.noreply.github.com>
2025-06-06 20:05:16 -0300

    att

---
+++ RevisaoJava/src/EstrRepeticao2/Exercicio03.java
@@ -0,0 +1,17 @@
+/*Faça um programa que leia e valide as seguintes informações:
+a. Nome: maior que 3 caracteres;
+b. Idade: entre 0 e 150;
+c. Salário: maior que zero;
+d. Sexo: 'f' ou 'm';
+e. Estado Civil: 's', 'c', 'v', 'd';*/
+
+package EstrRepeticao2;
+
+public class Exercicio03 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
